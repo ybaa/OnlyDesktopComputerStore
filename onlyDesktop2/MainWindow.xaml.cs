8ddd4563ad0a92b468c5ccab2283dd4b4b9ebad3
Martyna Łagożna <m.lagozna@live.com>
2017-01-07 11:31:38 +0100

    connection with database and show all product works

---
+++ onlyDesktop2/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.ObjectModel;
+
+namespace onlyDesktop2 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+
+
+        private void showAllProductsButton_Click(object sender, RoutedEventArgs e) {
+            ListViewItem lvl = new ListViewItem();
+            List<Products> products = new List<Products>();
+
+
+            SqlConnection conn = new SqlConnection("Data Source=MARTYNA-PC;Initial Catalog=SklepKomputerowy;Integrated Security=True");
+
+            SqlCommand command = new SqlCommand("SELECT * FROM Produkty", conn);
+            try {
+                conn.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                while (reader.Read()) {
+                    string ID = reader["ID_produktu"].ToString();
+                    string name = reader["Nazwa_produktu"].ToString();
+                    string type = reader["Typ_produktu"].ToString();
+                    string description = reader["Opis_produktu"].ToString();
+                    decimal price = 0;
+
+                    products.Add(new Products() { ID = ID, name = name, type = type, description = description, price = price });
+
+                }
+
+            }
+            catch (SqlException) {
+
+            }
+
+            foreach (Products p in products) {
+                myListView.Items.Add(p);
+            }
+
+        }
+    }
+    
+}
---
+++ onlyDesktop2/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onlyDesktop2 {
+    class Products {
+        public string ID { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        //public decimal buyPrice { get; set; }
+        //public string codeID { get; set; }
+    }
+}
