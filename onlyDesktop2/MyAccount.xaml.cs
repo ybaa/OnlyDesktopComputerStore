412d80c6b45a0b00bdd5758afc8579316f1491b1
Martyna Łagożna <m.lagozna@live.com>
2017-01-10 14:40:34 +0100

    Edit and show my data works (instead of password)

--- onlyDesktop2/MyAccount.xaml.cs
+++ onlyDesktop2/MyAccount.xaml.cs
@@ -20,17 +20,53 @@ namespace onlyDesktop2 {
     public partial class MyAccount : Window {
         public MyAccount() {
             InitializeComponent();
+
+            SqlConnection conn =
+                new SqlConnection("Data Source=MARTYNA-PC;Initial Catalog=SklepKomputerowy;Integrated Security=True");
+            SqlCommand command =
+                new SqlCommand("select * from Adresy full outer join Klienci_Adresy on Adresy.ID_Adresu = Klienci_Adresy.ID_Adresu full outer join Klienci on Klienci.ID_klienta = Klienci_Adresy.ID_klienta where Klienci.ID_klienta =" +MainWindow.clientID, conn);
+
+            try {
+                conn.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    nameTextBox.Text = reader["Imie"].ToString();
+                    surnameTextBox.Text = reader["Nazwisko"].ToString();
+                    mailTextBox.Text = reader["Mail"].ToString();
+                    cityTextBox.Text = reader["Miejscowosc"].ToString();
+                    zipCodeTextBox.Text = reader["Kod_pocztowy"].ToString();
+                    streetTextBox.Text = reader["Ulica"].ToString();
+                    houseNumberTextBox.Text = reader["Nr_domu"].ToString();
+                    localNumberTextBox.Text = reader["Nr_mieszkania"].ToString();
+                }
+
+            }
+            catch (SqlException) {
+            }
+
+
+
+
         }
+
+        List<MyOrders> myOrders = new List<MyOrders>();
+        bool areMyOrdersShown = false;
 
         private void myOrdersButton_Click(object sender, RoutedEventArgs e) {
+            showOrHideMyData(Visibility.Collapsed);
             myListView.Visibility = Visibility.Visible;
-            showMyOrders();
+            showMyOrders(myOrders);
         }
 
-        public void showMyOrders() {
-            List<MyOrders> myOrders = new List<MyOrders>();
-            SqlConnection conn = new SqlConnection("Data Source=MARTYNA-PC;Initial Catalog=SklepKomputerowy;Integrated Security=True");
-            SqlCommand command = new SqlCommand("SELECT DISTINCT paki.ID_paczki, Zamowienia.Data_zlozenia, Zamowienia.Status_zamowienia, paki.Cena_calkowita   FROM Zamowienia FULL OUTER JOIN paki ON Zamowienia.ID_paczki = paki.ID_paczki full outer join Klienci on Klienci.ID_klienta = Zamowienia.ID_klienta   WHERE Klienci.ID_klienta = " + MainWindow.clientID, conn);
+        public void showMyOrders(List<MyOrders> myOrders) {
+            SqlConnection conn =
+                new SqlConnection("Data Source=MARTYNA-PC;Initial Catalog=SklepKomputerowy;Integrated Security=True");
+            SqlCommand command =
+                new SqlCommand(
+                    "SELECT DISTINCT paki.ID_paczki, Zamowienia.Data_zlozenia, Zamowienia.Status_zamowienia, paki.Cena_calkowita   FROM Zamowienia FULL OUTER JOIN paki ON Zamowienia.ID_paczki = paki.ID_paczki full outer join Klienci on Klienci.ID_klienta = Zamowienia.ID_klienta   WHERE Klienci.ID_klienta = " +
+                    MainWindow.clientID, conn);
 
             try {
                 conn.Open();
@@ -49,9 +85,79 @@ namespace onlyDesktop2 {
             catch (SqlException) {
             }
 
+            if (areMyOrdersShown == false)
+                showMyOrdersInListView(myOrders);
+        }
+
+        private void showMyOrdersInListView(List<MyOrders> myOrders) {
             foreach (MyOrders p in myOrders) {
                 myListView.Items.Add(p);
             }
+            areMyOrdersShown = true;
+
         }
+
+        private void editDataButton_Click(object sender, RoutedEventArgs e) {
+            endableEditing(true);
+        }
+
+        private void finishEditionButton_Click(object sender, RoutedEventArgs e) {
+            endableEditing(false);
+
+            string name = nameTextBox.Text;
+            string surname = surnameTextBox.Text;
+            string mail = mailTextBox.Text;
+            string city = cityTextBox.Text;
+            string zipCode = zipCodeTextBox.Text;
+            string street = streetTextBox.Text;
+            string houseNumber = houseNumberTextBox.Text;
+            string localNumber = localNumberTextBox.Text;
+
+            SqlConnection conn = new SqlConnection("Data Source=MARTYNA-PC;Initial Catalog=SklepKomputerowy;Integrated Security=True");
+            SqlCommand command = new SqlCommand("update Klienci set Imie = '" + name + "', Nazwisko = '" + surname +"', Mail = '"+ mail + "' where ID_klienta = " + MainWindow.clientID, conn);
+            SqlCommand command2 = new SqlCommand("update Adresy set Miejscowosc = '"+ city+"',Kod_pocztowy='"+zipCode+"',Ulica='"+street+"',Nr_domu="+houseNumber+ " ,Nr_mieszkania="+localNumber, conn);
+            try
+            {
+                conn.Open();
+                command.ExecuteNonQuery();
+                command2.ExecuteNonQuery();
+            }
+            catch (SqlException)
+            {
+              
+            }
+
+
+        }
+
+        private void myDataButton_Click(object sender, RoutedEventArgs e) {
+            showOrHideMyData(Visibility.Visible);
+            endableEditing(false);
+            myListView.Visibility = Visibility.Collapsed;
+        }
+
+        public void showOrHideMyData(Visibility x) {
+            nameTextBox.Visibility = x;
+            surnameTextBox.Visibility = x;
+            mailTextBox.Visibility = x;
+            cityTextBox.Visibility = x;
+            zipCodeTextBox.Visibility = x;
+            streetTextBox.Visibility = x;
+            houseNumberTextBox.Visibility = x;
+            localNumberTextBox.Visibility = x;
+        }
+
+        public void endableEditing(bool enable) {
+            nameTextBox.IsEnabled = enable;
+            surnameTextBox.IsEnabled = enable;
+            mailTextBox.IsEnabled = enable;
+            cityTextBox.IsEnabled = enable;
+            zipCodeTextBox.IsEnabled = enable;
+            streetTextBox.IsEnabled = enable;
+            houseNumberTextBox.IsEnabled = enable;
+            localNumberTextBox.IsEnabled = enable;
+        }
+
     }
+
 }
